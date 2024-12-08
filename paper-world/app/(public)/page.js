19437ee04f9083f4ffdb21067a8304a43881d7f6
matek10000 <mateusz.dybas@microsoft.wsei.edu.pl>
2024-12-08 19:40:24 +0100

    fixs

--- paper-world/app/(public)/page.js
+++ paper-world/app/(public)/page.js
@@ -1,18 +1,13 @@
 "use client";
 
-import Link from 'next/link';
 import './home.css'; // Import stylów tylko dla strony głównej
 
 export default function HomePage() {
   return (
     <main>
       <div className="container">
-        <div className="card">
-          <h1>Paper World</h1>
-          <Link href="/register" className="button">
-            Zacznij teraz
-          </Link>
-          <p>Masz już konto? <Link href="/login">Zaloguj się!</Link></p>
+        <div className="logo-container">
+          <img src="/logo.png" alt="Paper World Logo" className="logo" />
         </div>
       </div>
     </main>
